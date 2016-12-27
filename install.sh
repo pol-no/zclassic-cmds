d9e0321758b2fa9223d8d456e6a4a0c8cbb13ba1
fxminer <ann.fxminer@gmail.com>
2016-12-27 21:16:15 +0100

    Install fix

--- install.sh
+++ install.sh
@@ -5,13 +5,14 @@ if [[ -z $CONFIG ]]
 then
     CONFIG=~/.bashrc
 fi
+CONFIG_DIR=$(dirname $(readlink -f ${CONFIG}))
 
 if [[ `grep zclassic-cmds ${CONFIG} | wc -l` -eq 0 ]]
 then 
     echo "Installing zclassic-cmds in ${CONFIG}..."    
     CMDS_DIR=$(dirname $(readlink -f $0))
 
-    cat ${CMDS_DIR}/zclassic-cmds | sed -e "s;%CMDS_DIR%;${CMDS_DIR};g" > ~/zclassic-cmds
+    cat ${CMDS_DIR}/zclassic-cmds | sed -e "s;%CMDS_DIR%;${CMDS_DIR};g" > ${CONFIG_DIR}/zclassic-cmds
     echo "# zclassic-cmds
 . ~/zclassic-cmds" >> $CONFIG
 
